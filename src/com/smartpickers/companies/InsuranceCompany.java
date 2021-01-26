98a0922851862d4216ddff8e2cb213e996e4fa4e
Monisha <Moni@Gowthams-MacBook-Pro.local>
2021-01-26 01:02:00 -0500

    implemented insurance company call

--- src/com/smartpickers/companies/InsuranceCompany.java
+++ src/com/smartpickers/companies/InsuranceCompany.java
@@ -1,13 +1,35 @@
 package com.smartpickers.companies;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.smartpickers.stockdata.GetAPI;
 
 public class InsuranceCompany extends AbstractCompany {
+	// Create Static ticker names for now - this will eventually get loaded from a
+	// Databases in the coming weeks
+	
+	 static final String THEHARTFORD = "HIG";
+	 static final String TRAVELLERS = "TRV";
+	 static final String MASSMUTUAL = "MCI";
+	 static final String ALLSTATE = "ALL";
+	
+
+	// Creating ArrayList to hold Stock Tickers
+	List<String> insuranceCompanies;
 	
 	private GetAPI getApi;
 	
 	public InsuranceCompany(GetAPI getApi) {
 		this.getApi = getApi;
+		
+		// Initialize the list
+		insuranceCompanies = new ArrayList<String>();
+		insuranceCompanies.add(THEHARTFORD);
+		insuranceCompanies.add(TRAVELLERS);
+		insuranceCompanies.add(MASSMUTUAL);
+		insuranceCompanies.add(ALLSTATE);
 	}
 
 	@Override
@@ -46,9 +68,16 @@ public class InsuranceCompany extends AbstractCompany {
 	}
 
 	@Override
-	public void getstockDetails(String tickerName) {
-		// TODO Auto-generated method stub
-		
+	public void getstockDetails(String tickerName) throws IOException {
+		// get stock data from the API
+		System.out.println("Getting stock data for Insurance Stocks!");
+		if(!this.insuranceCompanies.contains(tickerName))
+			System.err.println("We cannot get a quote at this time! The problem is because the Company is not a Insurance Company!");
+		else
+		{
+			String getData = getApi.getApiData(tickerName);
+			System.out.println(getData);
+		}
 	}
 	
 
