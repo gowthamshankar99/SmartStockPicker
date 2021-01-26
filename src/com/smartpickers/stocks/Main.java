42215aa62b581bd9607f625dbe91ef5f674503b7
Monisha <Moni@Gowthams-MacBook-Pro.local>
2021-01-25 23:08:49 -0500

    added main method

---
+++ src/com/smartpickers/stocks/Main.java
@@ -0,0 +1,17 @@
+package com.smartpickers.stocks;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		// Get the Set of Questions
+		
+		Scanner scanner = new Scanner(System.in);
+		
+
+	}
+
+}
