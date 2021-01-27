ade35753b8a3974f3313563c164f7035b3fc315e
Monisha <Moni@Gowthams-MacBook-Pro.local>
2021-01-27 01:35:02 -0500

    push changes for adding junit testing

---
+++ src/com/smartpickers/test/InputTest.java
@@ -0,0 +1,14 @@
+package com.smartpickers.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InputTest {
+
+	@Test
+	public void testingInputData() {
+		fail("Not yet implemented");
+	}
+
+}
