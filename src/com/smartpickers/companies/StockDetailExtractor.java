5441bbffbee728e14b93ac21dffd39c56c3d0925
Monisha <Moni@Gowthams-MacBook-Pro.local>
2021-02-09 22:06:22 -0500

    add precondition post condition intent

--- src/com/smartpickers/companies/StockDetailExtractor.java
+++ src/com/smartpickers/companies/StockDetailExtractor.java
@@ -14,16 +14,27 @@ public class StockDetailExtractor<T extends AbstractCompany> {
     private GetAPI getAPI;
     
     public StockDetailExtractor(T industryObject, GetAPI getAPI) {  
+    	/*
+    	 * Constructor setting the incoming IndustryObject
+    	 * 
+    	 * The Industry Object can be one of the Technology, Insurance, Automobile, Healthcare classes
+    	 */
     	this.industryObject = industryObject;  
     	this.getAPI = getAPI;
     	}
     
 	/**
-	 * This Method doesnt need to be a abstract method - all stocks regadless of what industry they belong to - have the same P/E ratio calculation
+	 * This Method doesnt need to be a abstract method - all stocks regardless of what industry they belong to - have the same P/E ratio calculation
 	 * @returns a  double value
 	 */
+    /*
+     * PRE_CONDITION - tickerName should be a valid ticker for API to respond valid data
+     */
+    /*
+	 * POST_CONDITION - return the PERatio
+	 * GETAPI should be reachable and shouldnt be a null value
+	 */
 	public String getPERatio(String tickerArray) throws IOException, ParseException {
-		
 		
 		JSONParser parser = new JSONParser();
 		JSONObject obj = (JSONObject)parser.parse(getAPI.getApiData(tickerArray, "OVERVIEW"));
@@ -36,6 +47,13 @@ public class StockDetailExtractor<T extends AbstractCompany> {
 	 * @throws IOException 
 	 * @throws ParseException 
 	 * @returns a  double value
+	 */
+    /*
+     * PRE_CONDITION - tickerName should be a valid ticker for API to respond valid data
+     */
+	/*
+	 * POST_CONDITION - return the Earnings Per Share
+	 * GETAPI should be reachable and shouldnt be a null value
 	 */
 	
 	public String getEPS(String tickerArray) throws ParseException, IOException {
@@ -53,6 +71,17 @@ public class StockDetailExtractor<T extends AbstractCompany> {
 	 * @throws ParseException 
 	 * @returns a  double value
 	 */
+	
+	/*
+	 * Intent - Gets the price to Book ratio by communicating with the API
+	 */
+    /*
+     * PRE_CONDITION - tickerName should be a valid ticker for API to respond valid data
+     */
+	/*
+	 * POST_CONDITION - return the Price to Equity Ratio
+	 * GETAPI should be reachable and shouldnt be a null value
+	 */
 	public String getDEquityRatio(String tickerArray) throws ParseException, IOException {
 		JSONParser parser = new JSONParser();
 		JSONObject obj = (JSONObject)parser.parse(getAPI.getApiData(tickerArray, "OVERVIEW"));
@@ -64,6 +93,16 @@ public class StockDetailExtractor<T extends AbstractCompany> {
 	/**
 	 * This Method doesnt need to be a abstract method - all stocks regardless of what industry they belong to - have the same P/E ratio calculation
 	 * @returns a  double value
+	 */
+	/*
+	 * Intent - Gets the long term assets of a Company by communicating with the API
+	 */
+    /*
+     * PRE_CONDITION - tickerName should be a valid ticker for API to respond valid data
+     */
+	/*
+	 * POST_CONDITION - return the Long Term Assets
+	 * GETAPI should be reachable and shouldnt be a null value
 	 */
 	public String getLongTermAssets(String tickerArray) throws ParseException, IOException {
 		// totalNonCurrentAssets
@@ -81,6 +120,16 @@ public class StockDetailExtractor<T extends AbstractCompany> {
 	 * @throws ParseException 
 	 * @returns a  double value
 	 */
+	/*
+	 * Intent - Gets the short term assets of a Company by communicating with the API
+	 */
+    /*
+     * PRE_CONDITION - tickerName should be a valid ticker for API to respond valid data
+     */
+	/*
+	 * POST_CONDITION - return the ShortTermDebt
+	 * GETAPI should be reachable and shouldnt be a null value
+	 */
 	public String getShortTermAssets(String tickerArray) throws ParseException, IOException {
 		JSONParser parser = new JSONParser();
 		JSONObject obj = (JSONObject)parser.parse(getAPI.getApiData(tickerArray, "BALANCE_SHEET"));
@@ -89,7 +138,16 @@ public class StockDetailExtractor<T extends AbstractCompany> {
 		return shortTermAssets;
 	}
 	
-	
+	/*
+	 * Intent - Gets the short term Debt of a Company by communicating with the API
+	 */
+    /*
+     * PRE_CONDITION - tickerName should be a valid ticker for API to respond valid data
+     */
+	/*
+	 * POST_CONDITION - return the ShortTermDebt
+	 * GETAPI should be reachable and shouldnt be a null value
+	 */
 	public String shortTermDebt(String tickerArray) throws ParseException, IOException {
 		JSONParser parser = new JSONParser();
 		JSONObject obj = (JSONObject)parser.parse(getAPI.getApiData(tickerArray, "BALANCE_SHEET"));
@@ -98,6 +156,18 @@ public class StockDetailExtractor<T extends AbstractCompany> {
 		return shortTermDebt;
 	}
 	
+	/*
+	 * Intent - Gets the long term Debt of a Company by communicating with the API
+	 */
+	
+	/*
+	 * PRE_CONDITION - tickerName should be a valid ticker
+	 */
+	
+	/*
+	 * POST_CONDITION - return the longTermDebt
+	 * GETAPI should be reachable and shouldnt be a null value
+	 */
 	public String longTermDebt(String tickerArray) throws ParseException, IOException {
 		JSONParser parser = new JSONParser();
 		JSONObject obj = (JSONObject)parser.parse(getAPI.getApiData(tickerArray, "BALANCE_SHEET"));
